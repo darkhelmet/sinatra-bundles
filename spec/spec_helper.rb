482e4d0f8f98c39d74cc7c635dd38b995509fb95
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-02-27 00:25:09 -0700

    remove vendor and upgrade dep in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'rubygems'
-Gem.clear_paths
-ENV['GEM_HOME'] = File.expand_path(File.join(File.dirname(__FILE__), '..', 'vendor'))
 require 'spec'
 require 'spec/autorun'
 require 'rack/test'
 
-gem 'sinatra', '>= 0.10.1'
+gem 'sinatra', '>= 1.0.a'
 require 'sinatra/base'
 require 'sinatra/bundles'
 
-Spec::Runner.configure do |config|
-  config.include Rack::Test::Methods
-  config.include Sinatra::Bundles::Helpers
+Spec::Runner.configure do |c|
+  c.include Rack::Test::Methods
+  c.include Sinatra::Bundles::Helpers
 end
